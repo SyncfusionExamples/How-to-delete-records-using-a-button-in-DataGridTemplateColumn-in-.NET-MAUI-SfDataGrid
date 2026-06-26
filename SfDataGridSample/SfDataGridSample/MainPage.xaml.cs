e029663d9bb73223bfc65b7e3cc8a65874e1f710
Muthupandy-SF4685 <muthupandy.ramakrishnan@syncfusion.com>
2026-06-26 12:59:50 +0530

    Added Demo sample and update details in ReadMe file.

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (sender is not Button btn)
+                return;
+
+            var parameter = btn.CommandParameter;
+
+            bool confirm = await DisplayAlert("Confirm Delete", "Are you sure you want to delete this record?", "Delete", "Cancel");
+            if (!confirm)
+                return;
+
+                viewModel?.DeleteRecord?.Execute(parameter);
+        }
+    }
+}
